339c86c007978584274379ce928e2b5a26bd8695
Will DeBerry <willdeberry@gmail.com>
2015-02-25 04:16:54 +0100

    switch to using with open() instead of explicitly having to close the file

--- parse_config.py
+++ parse_config.py
@@ -5,20 +5,20 @@ OPTION_CHAR =  '='
 
 def parse_config(filename):
     options = {}
-    f = open(filename)
-    for line in f:
-        # First, remove comments:
-        if COMMENT_CHAR in line:
-            # split on comment char, keep only the part before
-            line, comment = line.split(COMMENT_CHAR, 1)
-        # Second, find lines with an option=value:
-        if OPTION_CHAR in line:
-            # split on option char:
-            option, value = line.split(OPTION_CHAR, 1)
-            # strip spaces:
-            option = option.strip()
-            value = value.strip()
-            # store in dictionary:
-            options[option] = value
-    f.close()
+    with open(filename) as f:
+        for line in f:
+            # First, remove comments:
+            if COMMENT_CHAR in line:
+                # split on comment char, keep only the part before
+                line, comment = line.split(COMMENT_CHAR, 1)
+            # Second, find lines with an option=value:
+            if OPTION_CHAR in line:
+                # split on option char:
+                option, value = line.split(OPTION_CHAR, 1)
+                # strip spaces:
+                option = option.strip()
+                value = value.strip()
+                # store in dictionary:
+                options[option] = value
+
     return options
